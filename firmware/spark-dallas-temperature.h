b872fe1c29ea04fe3ece34f1878f9f3be9896202
cdrodriguez <cdrodriguez@users.noreply.github.com>
2015-06-07 22:16:59 -0700

    Update spark-dallas-temperature.h

--- firmware/spark-dallas-temperature.h
+++ firmware/spark-dallas-temperature.h
@@ -24,7 +24,7 @@
     #include <OneWire.h>
 #elif defined(SPARK) or defined(STM32F10X_MD)
     #include "application.h"
-    #include "../OneWire_PhotonOrCore/OneWire_PhotonOrCore.h"
+    #include "../OneWire-Photon/OneWire-Photon.h"
 #endif
 
 
