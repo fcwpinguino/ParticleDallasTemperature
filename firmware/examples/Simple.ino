a96edfca463351aa8add2fa0141b81ee212917a0
cdrodriguez <cdrodriguez@users.noreply.github.com>
2015-06-13 06:38:51 -0700

    Update Simple.ino

--- firmware/examples/Simple.ino
+++ firmware/examples/Simple.ino
@@ -2,7 +2,7 @@
 #include "particle-dallas-temperature/particle-dallas-temperature.h"
 
 // Data wire is plugged into port 2 on the Arduino
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS D4
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
